ba2f803d3fc8128c9b35fb545da2981e219e32e6
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-08-19 19:10:23 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+
+// global var
 let btnNav = document.getElementById("btn-nav")
 let offcanvasNav = document.getElementById("offcanvas")
 let iconCloseBtn = document.getElementById("icon-close-btn")
 let closeNaveText = document.querySelector(".fa-bars")
+// global var
 
 
+// navBar
 $(iconCloseBtn).click(function () {
     $(offcanvasNav).css("transform", "translateX(-100%)")
     $(btnNav).css("left", "0")
@@ -28,6 +32,18 @@ $(btnNav).click(function () {
         $(closeNaveText).css("animation","none ")
     }
 })
+// navBar
+
+
+//  singers
+$("#singers p").not(":first").hide()
+
+$("#singers h2").click(function(){
+   $(this).next().slideToggle(600)
+   $("p").not($(this).next()).slideUp(600)
+})
+//  singers
+
 
 
 
