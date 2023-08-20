7a05bd7e07c0a895de9f8de4b676b04c5d2992b1
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-08-20 21:06:39 +0300

    update

--- main.js
+++ main.js
@@ -1,106 +1,104 @@
-window.addEventListener("load",function(){
 
 
-    // loading
+
+    // loading spiner
     $(document).ready(function () {
-        $("#loading").fadeOut(2000)
+        $("#loading").fadeOut(4000)
     });
-    // loading
-    
-// global var
-let btnNav = document.getElementById("btn-nav")
-let offcanvasNav = document.getElementById("offcanvas")
-let iconCloseBtn = document.getElementById("icon-close-btn")
-let singersSec = document.getElementById("singers")
-let closeNavText = document.querySelector(".fa-bars")
-let textArea=document.getElementById("textarea")
-let lettersLeft=document.getElementById("lettersleft")
-let navLink=document.querySelectorAll("#offcanvas a")
+    // loading spiner
 
-// global var
+    // global var
+    let btnNav = document.getElementById("btn-nav")
+    let offcanvasNav = document.getElementById("offcanvas")
+    let iconCloseBtn = document.getElementById("icon-close-btn")
+    let singersSec = document.getElementById("singers")
+    let closeNavText = document.querySelector(".fa-bars")
+    let textArea = document.getElementById("textarea")
+    let lettersLeft = document.getElementById("lettersleft")
+    let navLink = document.querySelectorAll("#offcanvas a")
 
-// navBar
-$(iconCloseBtn).click(function () {
-    $(offcanvasNav).css("transform", "translateX(-100%)")
-    $(btnNav).css("left", "0")
-    $(iconCloseBtn).css("animation", "none ")
-})
-$(btnNav).click(function () {
-    $(iconCloseBtn).css("animation", "spin 2s 1s ")
-    let offcanvasWidth = $(offcanvasNav).css("width")
-    if ($(this).css("left") == "0px") {
-        $(offcanvasNav).css("transform", "none")
-        $(this).css("left", offcanvasWidth)
-        $(iconCloseBtn).css("animation", "spin 2s .8s ")
-        $(closeNavText).css("animation", "spin 2s  ")
+    // global var
 
-    } else {
+    // navBar
+    $(iconCloseBtn).click(function () {
         $(offcanvasNav).css("transform", "translateX(-100%)")
-        $(this).css("left", "0")
+        $(btnNav).css("left", "0")
         $(iconCloseBtn).css("animation", "none ")
-        $(closeNavText).css("animation", "none ")
+    })
+    $(btnNav).click(function () {
+        $(iconCloseBtn).css("animation", "spin 2s 1s ")
+        let offcanvasWidth = $(offcanvasNav).css("width")
+        if ($(this).css("left") == "0px") {
+            $(offcanvasNav).css("transform", "none")
+            $(this).css("left", offcanvasWidth)
+            $(iconCloseBtn).css("animation", "spin 2s .8s ")
+            $(closeNavText).css("animation", "spin 2s  ")
+
+        } else {
+            $(offcanvasNav).css("transform", "translateX(-100%)")
+            $(this).css("left", "0")
+            $(iconCloseBtn).css("animation", "none ")
+            $(closeNavText).css("animation", "none ")
+        }
+    })
+    // navBar
+
+
+    //  singers
+    $("#singers p").not(":first").hide()
+    $("#singers h2").click(function () {
+        $(this).next().slideToggle(600)
+        $("#singers p").not($(this).next()).slideUp(600)
+    })
+    //  singers
+
+
+    // duration countdown
+    let end = new Date('08/19/2024 10:10 PM');
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    function remianing() {
+        let now = new Date();
+        let distance = end - now;
+        let days = Math.floor(distance / day)
+        let hours = Math.floor((distance % day) / hour)
+        let mins = Math.floor((distance % hour) / minute)
+        let seconds = Math.floor((distance % minute) / second)
+        document.getElementById("days").innerHTML = days + " D"
+        document.getElementById("hours").innerHTML = hours + " H"
+        document.getElementById("mins").innerHTML = mins + " M"
+        document.getElementById("seconds").innerHTML = seconds + " S"
+
+        if (distance < 0) {
+            clearInterval(timer)
+            document.getElementById("days").innerHTML = "EXPIRED"
+            document.getElementById("hours").innerHTML = "EXPIRED"
+            document.getElementById("mins").innerHTML = "EXPIRED"
+            document.getElementById("seconds").innerHTML = "EXPIRED"
+        }
     }
-})
-// navBar
+    let timer = setInterval(remianing, 1000)
+    // duration  countdown
+
+    // textarea letters
+    textArea.addEventListener("keyup", function () {
+        let textAreaLength = (textArea.value).length
+        let leftLatters = 100 - textAreaLength
+        lettersLeft.innerHTML = leftLatters
+    })
+    // textarea letters
 
 
-//  singers
-$("#singers p").not(":first").hide()
-$("#singers h2").click(function () {
-    $(this).next().slideToggle(600)
-    $("#singers p").not($(this).next()).slideUp(600)
-})
-//  singers
-
-
-// duration countdown
-let end = new Date('08/19/2024 10:10 PM');
-let second = 1000;
-let minute = second * 60;
-let hour = minute * 60;
-let day = hour * 24;
-
-function remianing() {
-    let now = new Date();
-    let distance = end - now;
-    let days = Math.floor(distance / day)
-    let hours = Math.floor((distance % day) / hour)
-    let mins = Math.floor((distance % hour) / minute)
-    let seconds = Math.floor((distance % minute) / second)
-    document.getElementById("days").innerHTML = days + " D"
-    document.getElementById("hours").innerHTML = hours + " H"
-    document.getElementById("mins").innerHTML = mins + " M"
-    document.getElementById("seconds").innerHTML = seconds + " S"
-
-    if (distance < 0) {
-        clearInterval(timer)
-        document.getElementById("days").innerHTML = "EXPIRED"
-        document.getElementById("hours").innerHTML = "EXPIRED"
-        document.getElementById("mins").innerHTML = "EXPIRED"
-        document.getElementById("seconds").innerHTML = "EXPIRED"
-    }
-}
-let timer = setInterval(remianing, 1000)
-// duration  countdown
-
-// textarea letters
-textArea.addEventListener("keyup",function(){
-   let textAreaLength= (textArea.value).length
-let leftLatters=100-textAreaLength
-lettersLeft.innerHTML=leftLatters
-})
-// textarea letters
-
-
-// change window scroll speed
-$(navLink).on('click', function() {
-    var target = $( $(this).attr('href') );
-        $('html, body').animate({ scrollTop: target.offset().top},2000,false);
-});
-// change window scroll speed
+    // change window scroll speed
+    $(navLink).on('click', function () {
+        var target = $($(this).attr('href'));
+        $('html, body').animate({ scrollTop: target.offset().top }, 2000, false);
+    });
+    // change window scroll speed
 
 
 
 
-
-})
